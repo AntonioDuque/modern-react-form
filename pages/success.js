16f55c24f1fc9b9e04c73a7e49b32e72eb01dd7e
Antonio Duque <antonio.duque@hyd.com.es>
2023-01-04 15:19:32 +0100

    first commit

---
+++ pages/success.js
@@ -0,0 +1,47 @@
+import { useRouter } from "next/router";
+import { motion as m } from 'framer-motion'
+import Confetti from "react-confetti";
+import { useEffect, useState } from "react";
+
+
+export default function Success() {
+
+    const [pieces, setPieces] = useState(200);
+
+    const stopConfetti = () => {
+        setTimeout(()=> {
+            setPieces(0)
+        },3000);
+    };
+
+    useEffect(()=>{
+        stopConfetti();
+    },[]);
+
+
+    const router = useRouter();
+
+
+    return (
+        <m.main 
+        initial={{opacity:0}} 
+        animate={{opacity:1}} 
+        exit={{opacity:0}}
+        className="h-screen flex items-center justify-center"
+        >
+
+            <div className="bg-white rounded-lg w-1/2 font-bold text-gray-700 text-center p-16">
+                <h1 className="text-3xl pb-4">
+                    Thanks for the email  {router.query.name} 🚀
+                </h1>
+                <p className="text-lg  text-gray-500">
+                    We have sent you an email over at {router.query.email}. We will get
+                    back to you as soon as we can!
+                </p>
+            </div>
+
+            <Confetti gravity={0.2} numberOfPieces={pieces}/>
+
+        </m.main>
+    )
+}
